7442f7088e445fc40c55b8e3dd73761d16b1abb8
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-08-10 02:00:03 +0100

    add gatsby-plugin-layout

---
+++ packages/gatsby-plugin-layout/src/wrap-page.js
@@ -0,0 +1,11 @@
+const React = require(`react`)
+
+const preferDefault = m => (m && m.default) || m
+const Layout = preferDefault(require(GATSBY_LAYOUT_COMPONENT_PATH).default)
+
+// eslint-disable-next-line react/prop-types,react/display-name
+module.exports = ({ component, props }) => (
+  <Layout thisIsLayout {...props}>
+    {component}
+  </Layout>
+)
