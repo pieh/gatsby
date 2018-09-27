1df920a542a7a53f269637dbd36c75731ba16c32
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-09-27 14:13:55 +0200

    test

--- packages/gatsby/src/utils/cache.js
+++ packages/gatsby/src/utils/cache.js
@@ -2,10 +2,10 @@ const Promise = require(`bluebird`)
 const fs = require(`fs-extra`)
 const _ = require(`lodash`)
 
-const b =  "test2`"
-const c =  `test2`
+const b = 'test2'
+const c = `test2`
 
-const d =       `eh?`
+const d =    `eh?`
 
 const p = {
   a: 5,
