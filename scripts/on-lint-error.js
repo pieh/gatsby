2b6f29efb46e8f2fda530465ed4d628ce1bcc3c5
Michal Piechowiak <misiek.piechowiak@gmail.com>
2018-11-06 16:11:50 +0100

    don't reject commits and provide some feedback message

---
+++ scripts/on-lint-error.js
@@ -0,0 +1,4 @@
+// TODO: make this message nicer
+console.log(
+  `There were linting errors in your commit, you can fix them and run "git commit --amend"\nTo disable git hooks you can run "yarn run hooks:uninstall"\n`
+)
