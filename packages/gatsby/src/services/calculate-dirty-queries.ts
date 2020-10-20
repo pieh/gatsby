e677bcecbdbb8502619fd5efb8ff6b2a66a3250a
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-10-20 17:35:02 +0200

    only filter queries in develop

--- packages/gatsby/src/services/calculate-dirty-queries.ts
+++ packages/gatsby/src/services/calculate-dirty-queries.ts
@@ -12,27 +12,32 @@ export async function calculateDirtyQueries({
 }> {
   assertStore(store)
 
-  // 404 are special cases in our runtime that ideally use
-  // generic things to work, but for now they have special handling
-  const filter = new Set([`/404.html`, `/dev-404-page/`])
-  if (websocketManager?.activePaths) {
-    websocketManager.activePaths.forEach(filter.add.bind(filter))
-  }
-  const pendingPaths = websocketManager?.pendingPathPromises?.keys()
-  if (pendingPaths) {
-    for (const path of pendingPaths) {
-      filter.add(path)
-    }
-  }
-
   const state = store.getState()
   const queryIds = calcDirtyQueryIds(state)
-  reporter.verbose(`Filter: ${JSON.stringify(Array.from(filter))}`)
+  let queriesToRun: Array<string> = queryIds
+
   reporter.verbose(`Dirty queries: ${JSON.stringify(Array.from(queryIds))}`)
 
-  const queriesToRun = queryIds.filter(
-    queryId => filter.has(queryId) || queryId.startsWith(`sq--`)
-  )
+  if (process.env.gatsby_executing_command === `develop`) {
+    // 404 are special cases in our runtime that ideally use
+    // generic things to work, but for now they have special handling
+    const filter = new Set([`/404.html`, `/dev-404-page/`])
+    if (websocketManager?.activePaths) {
+      websocketManager.activePaths.forEach(filter.add.bind(filter))
+    }
+    const pendingPaths = websocketManager?.pendingPathPromises?.keys()
+    if (pendingPaths) {
+      for (const path of pendingPaths) {
+        filter.add(path)
+      }
+    }
+
+    reporter.verbose(`Filter: ${JSON.stringify(Array.from(filter))}`)
+
+    queriesToRun = queryIds.filter(
+      queryId => filter.has(queryId) || queryId.startsWith(`sq--`)
+    )
+  }
 
   reporter.verbose(`Queries to run: ${JSON.stringify(queriesToRun)}`)
 
